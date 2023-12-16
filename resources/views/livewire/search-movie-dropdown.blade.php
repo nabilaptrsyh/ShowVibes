399211f9684e7e51828991a8f9ea73333fcd21c4
Macbook <macbook@michael.local>
2023-12-16 14:39:56 +0700

    menambahkan tailwind css plugins

--- resources/views/livewire/search-movie-dropdown.blade.php
+++ resources/views/livewire/search-movie-dropdown.blade.php
@@ -12,6 +12,9 @@
             <path class="heroicon-ui" d="M16.32 14.915.39 5.4a1 1 0 01-1.42 1.41-5.38-5.38a8 8 0 111.41-1.41zM10 16a6 6 0 100-12 6 6 0 000 12z"/>
         </svg>
     </div>
+
+    <div class="spinner"></div>
+
     @if(strlen($search) > 2)
         <div class="absolute bg-gray-800 rounded w-64 mt-4">
             <ul>
