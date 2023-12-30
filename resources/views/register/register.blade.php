3ee81c8eb0e72f8c8c16b60a01da2c65613f8882
Rezadwipranata <rezadwi.rdp72@gmail.com>
2023-12-30 21:49:39 +0700

    registrasi function 1.0

--- resources/views/register/register.blade.php
+++ resources/views/register/register.blade.php
@@ -54,6 +54,7 @@
                       </div>
                       @enderror
                     </div>
+
                     <div class="form-floating">
                       <input type="text" name="username" class="form-control @error('username') is-invalid @enderror" id="username" placeholder="username" required value="{{ old('username') }}">
                       <label for="username">Username</label>
@@ -63,6 +64,7 @@
                       </div>
                       @enderror
                     </div>
+
                     <div class="form-floating">
                         <input type="email" name="email" class="form-control @error('email') is-invalid @enderror" id="email" placeholder="name@example.com" required value="{{ old('email') }}">
                         <label for="email">Email address</label>
@@ -72,6 +74,7 @@
                         </div>
                         @enderror
                     </div>
+                    
                     <div class="form-floating">
                       <input type="password" name="password" class="form-control rounded-bottom @error('password') is-invalid @enderror" id="password" placeholder="Password" required>
                       <label for="password">Password</label>
