b51bac07c15275a63b0f1c975bb5ed12be78d908
nabilaptrsyh <lllalanabila300@gmail.com>
2024-01-02 00:33:04 +0700

    Menyelesaikan login

--- app/Http/Controllers/GoogleController.php
+++ app/Http/Controllers/GoogleController.php
@@ -17,65 +17,42 @@ class GoogleController extends Controller
 
     public function handleGoogleCallback()
     {
-
-        $user = Socialite::driver('google')->user();
-        
-        $findUser = User::where('google_id', $user->getId())->first();
-
-        
-
-        if ($findUser) {
-                    Auth::login($findUser);
-                    dd($user);
-                    return redirect()->intended('home');
-                } else {
-                    $newUser = User::create([
-                        'name' => $user->getName(),
-                        'email' => $user->getEmail(),
-                        'google_id' => $user->getid(),
-                        'password' => bcrypt('123456')
-                        // tambahkan data lainnya sesuai kebutuhan
-                     ]);
-
-                     Auth::login($newUser);
-                     return redirect()->intended('home');
-                }
-
-        // try {
-        //     $user = Socialite::driver('google')->user();
+        try {
+            $user = Socialite::driver('google')->user();
     
-        //     // Cek apakah pengguna sudah terdaftar
-        //     $findUser = User::where('google_id', $user->getId())->first();
-        //     if ($findUser) {
-        //         Auth::login($findUser);
-        //     } else {
-        //         // Jika belum terdaftar, buat pengguna baru
-        //         $newUser = User::create([
-        //             'name' => $user->name,
-        //             'email' => $user->email,
-        //             'google_id' => $user->id,
-        //             'password' => bcrypt('123456')
-        //             // tambahkan data lainnya sesuai kebutuhan
-        //         ]);
+            // Cek apakah pengguna sudah terdaftar
+            $findUser = User::where('google_id', $user->getId())->first();
+            if ($findUser) {
+                Auth::login($findUser);
+            } else {
+                // Jika belum terdaftar, buat pengguna baru
+                $newUser = User::create([
+                    'name' => $user->name,
+                    'email' => $user->email,
+                    'google_id' => $user->id,
+                    'password' => bcrypt('123456')
+                    // tambahkan data lainnya sesuai kebutuhan
+                ]);
     
-        //         Auth::login($newUser);
-        //     }
+                Auth::login($newUser);
+            }
     
-        //     // Periksa nilai is_admin
-        //     $isAdmin = Auth::user()->is_admin;
+            return redirect()->intended('movies');
+            // Periksa nilai is_admin
+            // $isAdmin = Auth::user()->is_admin;
     
-        //     // Debugging: Pemantauan nilai $isAdmin
+            // Debugging: Pemantauan nilai $isAdmin
     
-        //     // Periksa apakah pengguna admin dan arahkan ke dashboard jika iya
-        //     if ($isAdmin == 1) {
-        //         return redirect()->route('dashboard.content.index');
-        //     } else {
-        //         return redirect()->intended('/');
-        //     }
+            // Periksa apakah pengguna admin dan arahkan ke dashboard jika iya
+            // if ($isAdmin == 1) {
+            //     return redirect()->route('movies');
+            // } else {
+            //     return redirect()->intended('movies');
+            // }
     
-        // } catch (\Exception $e) {
-        //     return redirect()->route('login')->with('error', 'Terjadi kesalahan saat menghandle callback: ' . $e->getMessage());
-        // }
+        } catch (\Exception $e) {
+            return redirect()->route('login')->with('error', 'Terjadi kesalahan saat menghandle callback: ' . $e->getMessage());
+        }
     }
 
 }
