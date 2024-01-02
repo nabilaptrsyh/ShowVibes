2a1d45338f3d80c016973e28d67ad3f70fc48b19
nabilaptrsyh <lllalanabila300@gmail.com>
2024-01-02 19:16:39 +0700

    BENERIN ADMIN

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -17,8 +17,8 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/admin';
-    public const HOMEUSER = '/Movie';
+    public const HOME = '/dashboard';
+    public const HOMEUSER = '/admin';
 
     /**
      * Define your route model bindings, pattern filters, and other route configuration.
