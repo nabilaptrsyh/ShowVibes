b284c9ad8a418b5fa35b99f77a95eb3692a7162d
nabilaptrsyh <lllalanabila300@gmail.com>
2024-01-03 13:37:08 +0700

    Fixing bug

--- resources/views/components/movie-card.blade.php
+++ resources/views/components/movie-card.blade.php
@@ -1,9 +1,9 @@
 <div class="mt-8">
-    <a href="{{ route('movies.show', $movie['id']) }}">
+    <a href="/movies/{{ $movie['id'] }}">
         <img src="{{ 'https://image.tmdb.org/t/p/w500/'.$movie['poster_path'] }}" alt="poster" class="hover:opacity-75 transition ease-in-out duration-150">
     </a>
     <div class="mt-2">
-        <a href="{{ route('movies.show', $movie['id']) }}" class="text-lg mt-2 hover:text-gray-300">{{ $movie['title'] }}</a>
+        <a href="/movies/{{ $movie['id'] }}" class="text-lg mt-2 hover:text-gray-300">{{ $movie['title'] }}</a>
         <div class="flex items-center text-gray-400 text-sm mt-1">
             <svg class="fill-current text-orange-500 w-4" viewBox="0 0 24 24"><g data-name="
             Layer 2"><path d="M17.56 21a1 1 0 01-.46-.11L12 18.22l-5.1 2.67a1 1 0
